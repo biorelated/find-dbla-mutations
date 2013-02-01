b2a605a9ffc910fcbb3146875d70386b76ae5ad5
georgeG <georgkam@gmail.com>
2013-02-01 14:41:14 +0300

    use bioruby alignment to parse multiple alignements

--- lib/selection/selection.rb
+++ lib/selection/selection.rb
@@ -1,67 +1,17 @@
-class Selection
 
-  def fasta_to_hash(file_path)
-    Bio::FlatFile.auto(file_path){ |f| f.map {|entry| Hash.[](entry.definition.to_sym,[entry.seq.to_s])} }  
-  end
+file = "#{ENV['HOME']}/BGI_data/cluster/cd-hit/98/cluster_1033/cluster_1033.aln"
+file = "#{ENV['HOME']}/BGI_data/cluster/cd-hit/98/cluster_550/cluster_550.aln"
 
-  #given an array return the indices of the most different elements
-  def index_of_least_identical(array)
-    freq_hash = array.inject(Hash.new(0)) { |k,v| k[v] += 1; k }
-    most_freq = array.sort_by { |v| freq_hash[v] }.last
-    array.each_index.map {|i| i+1 if array[i] != most_freq}
-  end
+require 'bio-alignment'
 
-  def get_codons(seq)
-    codons = []
-    seq.window_search(3,3) {|c| codons.push c}
-    codons
-  end
+require 'bio-alignment/bioruby' # make Bio::Sequence enumerable
+include Bio::BioAlignment
 
-  def get_aa(seq)
-    get_codons(seq).map {|codon| codon.translate}
-  end
+aln = Alignment.new
 
-  def get_seqs(seqs)
-    codon_arrays = []
-    seqs.each do |seq|
-      seq.each do |k,v|
-        bioseq = Bio::Sequence::NA.new(v.pop)
-        codon_arrays << get_codons(bioseq)
-      end
-    end
-    codon_arrays
-  end
-
-  def get_column_as_string(data,column)
-    #len = data.first.size
-    data.map{|item| item[column]}.join('')
-  end
+Bio::FlatFile.auto(file).each_entry do |entry|
+  aln << Sequence.new(entry.definition,entry.seq)
 end
-
-##file_path = "#{ENV['HOME']}/Batch2/batch2_cafs/pipeline2/example.fasta"
-#file_path = "#{ENV['HOME']}/BGI_data/analysis/clustering/cd-hit/98/cluster2489/cluster_2489.fasta"
-
-#selection = Selection.new
-
-#hash_data = selection.fasta_to_hash(file_path)
-
-#data  = selection.get_seqs(hash_data)
-
-#column_seq = selection.get_column_as_string(data,0)
-
-#bioseq = Bio::Sequence::NA.new(column_seq)
-
-#codons = selection.get_codons(bioseq)
-
-##puts codons.inspect
-
-#puts selection.get_aa(bioseq).inspect
-
-##puts selection.index_of_least_identical(codons).compact.inspect
-
-##create an instance of a standard codon table
-##t = Bio::CodonTable[1]
-##puts t.inspect
-##TODO detemine the whether a site is synonymous or non-synonymous
-
-
+aln.columns.each do |column|
+  puts column.to_s.split(//).inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+end
