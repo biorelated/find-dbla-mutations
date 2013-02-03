aa4ec47858b9b298624f56fc253946d9f1c5e086
georgeG <georgkam@gmail.com>
2013-02-03 22:29:35 +0300

    convert the code to a class

--- lib/selection/selection.rb
+++ lib/selection/selection.rb
@@ -1,17 +1,37 @@
-
-file = "#{ENV['HOME']}/BGI_data/cluster/cd-hit/98/cluster_1033/cluster_1033.aln"
-file = "#{ENV['HOME']}/BGI_data/cluster/cd-hit/98/cluster_550/cluster_550.aln"
-
 require 'bio-alignment'
-
 require 'bio-alignment/bioruby' # make Bio::Sequence enumerable
 include Bio::BioAlignment
 
-aln = Alignment.new
+class PopGen
 
-Bio::FlatFile.auto(file).each_entry do |entry|
-  aln << Sequence.new(entry.definition,entry.seq)
+  def initialize(file)
+    @file = file
+  end
+
+  def seq_al
+    aln = Alignment.new
+    Bio::FlatFile.auto(@file).each_entry do |entry|
+      aln << Sequence.new(entry.definition,entry.seq)
+    end
+    aln
+  end
+
+  #get nucleotide frequencies at each position
+  def nucleotide_freq
+    seq_al.columns.map do |column|
+      column.to_s.split(//).inject(Hash.new(0)) { |h,v| h[v] += 1; h } #.to_a.flatten.map{|c| c.join('')}
+    end
+  end
+
+  def entropy
+
+  end
+
 end
-aln.columns.each do |column|
-  puts column.to_s.split(//).inject(Hash.new(0)) { |h,v| h[v] += 1; h }
-end
+
+file = "#{ENV['HOME']}/BGI_data/cluster/cd-hit/98/cluster_550/cluster_550.aln"
+
+popgen = PopGen.new(file)
+
+p popgen.nucleotide_freq
+
