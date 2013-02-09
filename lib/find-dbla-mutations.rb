82ac0e217cc6a187181fc8237012061118a84e25
georgeG <georgkam@gmail.com>
2013-02-09 12:23:53 +0300

    comment the selection file out

--- lib/find-dbla-mutations.rb
+++ lib/find-dbla-mutations.rb
@@ -1,6 +1,6 @@
 require 'bio'
 require File.join(File.expand_path(File.dirname(__FILE__)), 'mutation','mutation')
-require File.join(File.expand_path(File.dirname(__FILE__)), 'selection','selection')
+#require File.join(File.expand_path(File.dirname(__FILE__)), 'selection','selection')
 
 #file_path = "#{ENV['HOME']}/Batch2/batch2_cafs/pipeline2/example.fasta"
 
