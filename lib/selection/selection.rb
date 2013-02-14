d3d8ad880c2a378e2f88f78fe9f5863451c9ea4a
georgeG <georgkam@gmail.com>
2013-02-14 14:44:21 +0300

    keep the functionality in a different file

--- lib/selection/selection.rb
+++ lib/selection/selection.rb
@@ -39,27 +39,6 @@ class PopGen
       cees = column.to_s.count('C')
       tees = column.to_s.count('T')
       "#{gees},#{aaas},#{cees},#{tees}"
-      #column.to_s  #.split(//).inject(Hash.new(0)) { |h,v| h[v] += 1; h }
     end
   end
-
-  def entropy
-
-  end
-end
-
-file = "#{ENV['HOME']}/BGI_data/cluster/cd-hit/98/cluster_550/cluster_550.aln"
-#file = "#{ENV['HOME']}/BGI_data/cluster/cd-hit/88/cluster_1290/cluster_1290.aln"
-#file = "#{ENV['HOME']}/BGI_data/cluster/cd-hit/var1/98/cluster_1/cluster_1.aln"
-
-popgen = PopGen.new(file)
-
-#popgen.nucleotide_freq.each_with_index do |c,index|
-  #puts "#{index + 1},#{c}"
-#end
-
-popgen.print_codons.each_with_index do |col,index|
-  star = '*' if col.to_s.scan(/.{3}|.+/).uniq.size > 1
-  amino = Bio::Sequence::NA.new(col.to_s).translate.split(//).uniq.join('')
-  puts "#{index + 1} #{col.to_s.scan(/.{3}|.+/).join(' ')} #{amino} #{star}"
 end
